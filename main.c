fdb272cffe3a3dfb59ce4bee445de659f4c46e5b
Alberto Petrucci <capriele93@hotmail.it>
2017-02-03 11:19:17 +0100

    Aggiunto il supporto al salvataggio di un buffer di campioni sulla penna USB (FAT32) semplicemente alla pressione dello Switch1 (SW1)

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+
 #include <stdbool.h>
 #include <stdint.h>
 #include <math.h>
@@ -36,17 +37,25 @@
 #include "driverlib/i2c.h"
 
 //USB
-#include "usbDiskController.h"
+#define EXECUTE_USB 1
+#if EXECUTE_USB
+	#include "usbDiskController.h"
+	#define ADC_BUF_SIZE 512
+	#define LED_GREEN 0x08
+	uint32_t ADC_BUF[ADC_BUF_SIZE];
+	int BUF_COUNT = 0;
+	static int ADC_BUFFER_STORE_ENABLE = 0;
+#endif
 
 //FFT
-#define EXECUTE_FFT 1
+#define EXECUTE_FFT 0
 #if EXECUTE_FFT
 	#include "fix_fft.h"
 	static fft_vector fft;
 #endif
 
 //FIR FILTER
-#define EXECUTE_FIR 0
+#define EXECUTE_FIR 1
 #if EXECUTE_FIR
 	#include "fir.h"
 	static FIR_filter fir;
@@ -65,7 +74,7 @@
 //#define ADC_SAMPLE_BUF_SIZE 128
 //uint16_t ADC_OUT[8];
 #define ADC_SAMPLE_FREQUENCY 192000//50000
-#define  ADC0_SEQUENCER0   0
+#define ADC0_SEQUENCER0   0
 #define ADC_SAMPLE_BUF_SIZE 1
 uint32_t ADC_OUT[ADC_SAMPLE_BUF_SIZE];
 
@@ -149,7 +158,7 @@ void ADC0SS1IntHandler(void) {
 	//SysCtlDelay(80000);
     /*FIR FILTER*/
     #if EXECUTE_FIR
-    FIR_put_sample(&fir, ADC_OUT[0]);
+    fir.valid = 1;
     #endif
 
     /*FFT*/
@@ -157,6 +166,14 @@ void ADC0SS1IntHandler(void) {
     fftAddSample(&fft, ADC_OUT[0]);
     #endif
 
+	#if EXECUTE_USB
+    //Come da chiacchierata con Walter iniziamo a riempire un buffer di 512 campioni.
+    if(ADC_BUFFER_STORE_ENABLE && BUF_COUNT < ADC_BUF_SIZE)
+    {
+        ADC_BUF[BUF_COUNT]=ADC_OUT[0];
+        BUF_COUNT++;
+    }
+	#endif
     //TimerEnable(TIMER0_BASE, TIMER_A);
 }
 
@@ -168,7 +185,7 @@ void Timer0IntHandler(void) {
 int newFile = 1;
 void SW1_IntHandler(void) {
 	if (GPIOIntStatus(GPIO_PORTF_BASE, false) & GPIO_PIN_4) {
-
+		/*
 		// Call the USB stack to keep it running.
 		USBHCDMain();
 
@@ -182,15 +199,50 @@ void SW1_IntHandler(void) {
 		//sprintf(text,"%d\n", (int)ADC_OUT[0]);
 		usbEnableCreateFile("samples.txt", text, sizeof(text), newFile);
 		newFile = 0;
+		*/
+		#if EXECUTE_USB
+        ADC_BUFFER_STORE_ENABLE = 1;
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, LED_GREEN);
+		#endif
 	}
 	GPIOIntClear(GPIO_PORTF_BASE, GPIO_INT_PIN_4);
 }
+#if EXECUTE_USB
+//Come da indicazione, iniziamo a leggere la funzione per il buffer
+void BUF_Reader(void){
+    int i = 0;
+    newFile = 1;
+    while(i < ADC_BUF_SIZE){
+        // Call the USB stack to keep it running.
+        USBHCDMain();
+
+        // PF4 was interrupt cause
+        //UARTprintf("SW1_IntHandler\n");
+        static char text[4+2] = "1234\n";
+        text[0] = (ADC_BUF[i]/1000)%10 +'0';
+        text[1] = (ADC_BUF[i]/100)%10 +'0';
+        text[2] = (ADC_BUF[i]/10)%10 +'0';
+        text[3] = ADC_BUF[i]%10 +'0';
+
+        //sprintf(text,"%d\n", (int)ADC_OUT[0]);
+        usbEnableCreateFile("samples.txt", text, sizeof(text), newFile);
+        usbDiskMainLoop();
+        newFile = 0;
+        ++i;
+	}
+    BUF_COUNT = 0;
+    ADC_BUFFER_STORE_ENABLE = 0;
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0x00);
+}
+#endif
 void InitSW1(void) {
+	//Abilito i led
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF); // Enable port F
+	GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3);
+
 	// Pin F4 setup
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);        // Enable port F
 	GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_4);  // Init PF4 as input
-	GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_4, GPIO_STRENGTH_2MA,
-			GPIO_PIN_TYPE_STD_WPU);  // Enable weak pullup resistor for PF4
+	GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_4, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);  // Enable weak pullup resistor for PF4
 
 	// Interrupt setup
 	GPIOIntDisable(GPIO_PORTF_BASE, GPIO_PIN_4); // Disable interrupt for PF4 (in case it was enabled)
@@ -200,6 +252,7 @@ void InitSW1(void) {
 	GPIOIntEnable(GPIO_PORTF_BASE, GPIO_PIN_4);     // Enable interrupt for PF4
 }
 
+static int sample;
 int main(void) {
 	SysCtlClockSet(SYSCTL_SYSDIV_2_5|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
 	//SysCtlClockSet(SYSCTL_SYSDIV_1|SYSCTL_USE_PLL|SYSCTL_OSC_MAIN|SYSCTL_XTAL_16MHZ);
@@ -219,26 +272,28 @@ int main(void) {
 	InitConsole();
 	ADCInit();
 
-
-	//SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
-	//GPIOPinTypeGPIOOutput(GPIO_PORTA_BASE, GPIO_PIN_3);
-	//GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_3, 0x00);
-
-	//InitSW1();
-	//usbDiskInit();
+	#if EXECUTE_USB
+	InitSW1();
+	usbDiskInit();
+	#endif
 
 	IntMasterEnable();
-
 	while(1)
 	{
-		//usbDiskMainLoop();
+		#if EXECUTE_USB
+		usbDiskMainLoop();
+		if(BUF_COUNT == ADC_BUF_SIZE){
+			BUF_Reader();
+		}
+		#endif
 
 		#if EXECUTE_FIR
-	    uint16_t sample = FIR_get_sample(&fir);
 		if(fir.valid){
+			FIR_put_sample(&fir, ADC_OUT[0]);
+			sample = FIR_get_sample(&fir);
 			fir.valid = 0;
-			UARTprintf("%d", fir.last_filtered_sample);
-			UARTprintf("\n");
+			//UARTprintf("%d", fir.last_filtered_sample);
+			//UARTprintf("\n");
 		}
 		#endif
 
@@ -256,7 +311,11 @@ int main(void) {
 
 	    /*SPI DAC*/
 		#if EXECUTE_DAC_OUTPUT
-	    SpiDACWrite(&dac, (uint16_t)ADC_OUT[0]);
+			#if EXECUTE_FIR
+			SpiDACWrite(&dac, (uint16_t)sample);
+			#else
+	    		SpiDACWrite(&dac, (uint16_t)ADC_OUT[0]);
+			#endif
 		#endif
 	}
 }
